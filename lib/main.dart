638910a99d541aaaea43b7748e22ee312dc76ffd
Kawsar Ahmed Shakil <kashakil75@gmail.com>
2022-12-12 21:02:09 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'loading.dart';
+import 'choose_location.dart';
+
+void main() => runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    ));
